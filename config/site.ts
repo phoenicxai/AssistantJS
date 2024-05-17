40112332cacc5365c1869c1c1a76303dc265ecf6
strategy-pnx <138083308+strategy-pnx@users.noreply.github.com>
2024-05-17 18:10:39 +0530

    project setup

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "AssistantJS",
+  description:
+    "Assistant Teaches Java Script",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/assistantjs",
+    github: "https://github.com/phoenicxai/assistantjs",
+    docs: "https://assistantjs.com",
+  },
+}
